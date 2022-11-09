7a7d837b3dae7f5630ec204606b2c397196869e2
Che-Wei Chang <changc@uni-hohenheim.de>
2022-11-09 18:40:36 +0100

    update detect_outlier_in_GeneticSpace.R: parallelize part of computation; modify the multistage test strategy

--- ggoutlier/R/detect_outlier_in_GeneticSpace.R
+++ ggoutlier/R/detect_outlier_in_GeneticSpace.R
@@ -4,7 +4,7 @@
 #' @param pgdM a pairwise genetic distance matrix. Users can provide a customized genetic distance matrix with this argument. Samples are ordered in rows and columns as in the rows of `geo_coord`. The default of `pgdM` is `NULL`. If `pgdM` is not provided, a genetic distance matrix will be calculated from `gen_coord`.
 #' @param k number of the nearest neighbor. the default is `NULL`.
 #' @param klim if `k = NULL`, an optimal k will be searched between the first and second value of `klim`
-#' @param s a scalar of geographical distance. The default `s=1000` scales the distance to a unit of 1 kilometer.
+#' @param s a scalar of geographical distance. The default `s=100` scales the distance to a unit of 1 kilometer.
 #' @param plot_dir the path to save plots
 #' @param w_power a value controlling the power of distance weight in KNN prediction. For example, if `w_power=2`, the weight of KNN is 1/d^2/sum(1/d^2).
 #' @param p_thres a significance level
@@ -41,7 +41,7 @@ ggoutlier_geneticspace <- function(geo_coord, gen_coord = NULL, pgdM = NULL,
                                            w_power = 2,
                                            p_thres = 0.05,
                                            n = 10^6,
-                                           s = 1000,
+                                           s = 100,
                                            multi_stages = T,
                                            maxIter=100,
                                            keep_all_stg_res = F,
@@ -55,11 +55,19 @@ ggoutlier_geneticspace <- function(geo_coord, gen_coord = NULL, pgdM = NULL,
     require(foreach)
     require(doParallel)
     max_cores=detectCores()
-    if(cpu >= max_cores){}
-    message()
+    if(cpu >= max_cores){
+        warning(paste0("\n The maximum number of CPUs is ", max_cores, ". Set `cpu` to ", max_cores-1," \n"))
+        cpu <- max_cores -1 # reserve max-1 cpus if users request too many cpus
+    }
+    if(cpu > 1){
+      message(paste0("\n parallelize computation using ", cpu, " cores \n"))
+      cl <- makeCluster(cpu)
+      registerDoParallel(cl)
+      do_par <- TRUE
+    }
   } else {
     if(cpu < 1){stop("`cpu` has to be at least 1")}
-    message()
+    message("\n computation using 1 core. you can parallelize computation by setting a higher value to `cpu` argument \n")
   }
 
   # check inputs
@@ -110,18 +118,13 @@ ggoutlier_geneticspace <- function(geo_coord, gen_coord = NULL, pgdM = NULL,
     pgdM <- pgdM + 10^-6
     diag(pgdM) <- 0
   }
-
+  #---------------------------------
   # search KNN
   ## a function to find KNN
   find_gen_knn <- function(pgdM, k){
     indx <- 1:ncol(pgdM)
     res <- matrix(NA, nrow = nrow(pgdM), ncol = k)
     for(i in indx){
-      #tmp.d <- cbind(indx, pgdM[,i])
-      #tmp.d <- tmp.d[order(tmp.d[,2], decreasing = F),]
-      # get the k nearest neighbors but with more than 0 unit dist
-      #res[i,] <- tmp.d[tmp.d[,2] > 0,][1:k,1]
-      #res[i,] <- tmp.d[tmp.d[,1] != i,][1:k,1]
       res[i,] <- FastKNN::k.nearest.neighbors(i = i, distance_matrix = pgdM, k = k)
     }
     return(res)
@@ -133,7 +136,7 @@ ggoutlier_geneticspace <- function(geo_coord, gen_coord = NULL, pgdM = NULL,
     for(j in 1:nrow(pgdM)){
       tmp.indx <- knn.indx[j,]
       tmp.geo_coord <- geo_coord[tmp.indx,]
-      tmp.d <- (pgdM[tmp.indx,j]) ^ w_power # new approach
+      tmp.d <- (pgdM[tmp.indx,j]) ^ w_power
       w <- (1/tmp.d)/(sum(1/tmp.d))
       res[j,] <- apply(tmp.geo_coord, 2, function(x){weighted.mean(x, w)})
     }
@@ -146,25 +149,39 @@ ggoutlier_geneticspace <- function(geo_coord, gen_coord = NULL, pgdM = NULL,
     return(geodist)
   } # cal_Dgeo end
 
-
+  ##---------------------------------------------------
+  ## search for the optimal K
   if(is.null(k)){
     # automatically select k if k=NULL
-    message(paste("searching for optimal k between", klim[1], "and", klim[2],"\nthis process can take a lot of time...\n"))
+    message(paste("\n searching for optimal k between", klim[1], "and", klim[2],"\nthis process can take time...\n"))
     k = min(klim)
     all.D <- c()
 
-    while(k <= max(klim)){
-      message(paste0("calculating Dgeo with k = ",k,"\r"), appendLF = F)
-      # find KNN
-      knn.indx <- find_gen_knn(pgdM, k=k)
-      # KNN predicttion
-      pred.geo_coord <- pred_geo_coord_knn(geo_coord, pgdM, knn.indx)
-
-      # calculate Dg statistic
-      Dgeo <- cal_Dgeo(pred.geo_coord = pred.geo_coord, geo_coord = geo_coord, scalar = s)
-      all.D <- c(all.D, sum(Dgeo))
-      k=k+1
+    ## parallel computation (if `cpu` > 1)
+    if(do_par){
+      kindx <- seq(from = k, to = max(klim), by = 1)
+      all.D <- foreach(k = kindx, .packages='geosphere', .combine="c") %dopar% {
+        knn.indx <- find_gen_knn(pgdM, k=k)
+        # KNN predicttion
+        pred.geo_coord <- pred_geo_coord_knn(geo_coord, pgdM, knn.indx)
+        # calculate Dg statistic
+        return(sum(cal_Dgeo(pred.geo_coord = pred.geo_coord, geo_coord = geo_coord, scalar = s)))
+      }
+    } else {
+      ## computation with a single cpu
+      while(k <= max(klim)){
+        message(paste0("calculating Dgeo with k = ",k,"\r"), appendLF = F)
+        # find KNN
+        knn.indx <- find_gen_knn(pgdM, k=k)
+        # KNN predicttion
+        pred.geo_coord <- pred_geo_coord_knn(geo_coord, pgdM, knn.indx)
+        # calculate Dg statistic
+        Dgeo <- cal_Dgeo(pred.geo_coord = pred.geo_coord, geo_coord = geo_coord, scalar = s)
+        all.D <- c(all.D, sum(Dgeo))
+        k=k+1
+      }
     }
+
     opt.k = c(klim[1]:klim[2])[which.min(all.D)]
     k.sel.plot <- paste(plot_dir, "/KNN_Dgeo_optimal_k_selection.pdf", sep = "")
     pdf(k.sel.plot, width = 5, height = 4)
@@ -175,7 +192,7 @@ ggoutlier_geneticspace <- function(geo_coord, gen_coord = NULL, pgdM = NULL,
     dev.off()
 
     k = opt.k
-    message(paste("the optimal k is ",opt.k,". the figure is saved at ", k.sel.plot," \n", sep = ""))
+    message(paste("\n the optimal k is ",opt.k,". the figure is saved at ", k.sel.plot," \n", sep = ""))
 
   }
   if(is.null(k)){stop("k is NULL!")}
@@ -186,16 +203,32 @@ ggoutlier_geneticspace <- function(geo_coord, gen_coord = NULL, pgdM = NULL,
   # calculate Dgeo statistic
   message(paste("D geo is scaled to a unit of",s,"meters \n"))
   Dgeo <- cal_Dgeo(pred.geo_coord = pred.geo_coord, geo_coord = geo_coord, scalar = s)
+
+  # rescale Dgeo
+  # -> maximum likelihood estimation would run into an error if the values of Dgeo is too large
+  # -> rescaling does not influence our outlier test
+  maxD <- max(Dgeo)
+  if(maxD > 20){
+    tmps <- 10
+    while (maxD > 10) {
+      tmps <- tmps * 10
+      maxD <- maxD / tmps
+    }
+    orig_s <- s
+    s <- s * tmps
+    message(paste0("\n\n GGoutlieR adjusts the given scalar `s` value from `s=", orig_s, "` to `s=", s, "` to prevent an error in the maximum likelihood estimation process\n"))
+  }
+  Dgeo <- Dgeo/tmps
   if(any(Dgeo == 0)){
     tmp.indx <- Dgeo == 0
-    message(paste(sum(tmp.indx),"samples have D geo = 0. Zeros are replaced with 10^-8 to avoid the error in maximum likelihood estimation."))
+    message(paste(sum(tmp.indx),"samples have D geo = 0. Zeros are replaced with 10^-8 to prevent the error in maximum likelihood estimation."))
     Dgeo[tmp.indx] <- 10^-8
   }
 
   predR2 <- diag(cor(pred.geo_coord, geo_coord))^2
   if(min(predR2) >= warning_minR2){
-    warning(paste0("\n\n\nThe lowest prediction accuracy (R^2) of KNN among two geographical dimensions (according to the given `geo_coord`) is ", round(min(predR2), digits = 4),
-                   ". Maybe only few extreme outliers in your samples. \nYou could manually check the results with `plot_GGNet_map` and adjust its `p_thres` argument to see which threshold is more appropriate.\n\n\n\n"))
+    warning(paste0("\n\n\n The lowest prediction accuracy (R^2) of KNN among two geographical dimensions (according to the given `geo_coord`) is ", round(min(predR2), digits = 4),
+                   ". Maybe only few extreme outliers in your samples. \nYou could manually check the results with `plot_GGoutlieR` and adjust its `p_thres` argument to see which threshold is more appropriate. \n\n\n\n"))
   }
   # get a null distribution
   ## Maximum likelihood assuming Gamma distribution
@@ -205,8 +238,8 @@ ggoutlier_geneticspace <- function(geo_coord, gen_coord = NULL, pgdM = NULL,
   }
 
   ### Loop until paramters converging
-  initial.a <- (mean(Dgeo)^2)/var(Dgeo)
-  initial.b <- mean(Dgeo)/var(Dgeo)
+  initial.a <- (mean(scaledDgeo)^2)/var(scaledDgeo)
+  initial.b <- mean(scaledDgeo)/var(scaledDgeo)
   message("Using maximum likelihood estimation to infer the null Gamma distribution...\n")
   mle.res <- mle(minuslogl = negLL, start = list(a = initial.a, b = initial.b),
                  lower = 10^-8, upper = 10^8, method = "L-BFGS-B")
@@ -246,41 +279,40 @@ ggoutlier_geneticspace <- function(geo_coord, gen_coord = NULL, pgdM = NULL,
   knn.name <- apply(knn.indx,2, function(x){rownames(geo_coord)[x]})
   res.out <- list(out, thres, gamma.par, knn.indx, knn.name, s)
   names(res.out) <- c("statistics","threshold","gamma_parameter", "knn_index", "knn_name", "scalar")
-
+  attr(res.out, "model") <- "ggoutlier_GeneticSpace"
 
   if(!multi_stages){
     return(res.out)
   }else{
-    # multi-stage testing
+    # multi-stage test
     message(paste0("Start multi-stage KNN test process with k=",k,
                    " and using the null Gamma distribution with shape=", round(current.a, digits = 3),
                    " and rate=",round(current.b, digits = 3),
                    " (the parameters of Gamma distribution were determined by MLE)\n"))
 
     i=1
-    declining_rate = 0.8
-    to_keep <- res.out$statistics$p.value >= p_thres*(1-declining_rate^i)
+    to_keep <- res.out$statistics$p.value > min(res.out$statistics$p.value)
     tmp.pgdM <- pgdM[to_keep, to_keep]
     tmp.geo_coord <- geo_coord[to_keep,]
 
     res.Iters <- list(res.out)
-    while (i <= maxIter) {
+    new_s = s*DgeoSD
+    system.time(while (i <= maxIter) {
       if(i > 1){
         tmp.pgdM <- tmp.pgdM[to_keep, to_keep]
         tmp.geo_coord <- tmp.geo_coord[to_keep,]
       }
 
       message(paste0("Iteration ", i,"\r"), appendLF = F)
-
-
       # find KNN
       tmp.knn.indx <- find_gen_knn(tmp.pgdM, k=k)
       # KNN predicttion
       tmp.pred.geo_coord <- pred_geo_coord_knn(tmp.geo_coord, tmp.pgdM, tmp.knn.indx)
       # calculate Dg statistic
-      tmp.Dgeo <- cal_Dgeo(pred.geo_coord = tmp.pred.geo_coord, geo_coord = tmp.geo_coord, scalar = s)
+      tmp.Dgeo <- cal_Dgeo(pred.geo_coord = tmp.pred.geo_coord, geo_coord = tmp.geo_coord, scalar = new_s)
+      # NOTE: `tmp.Dgeo` has to be divided by `DgeoSD` because the null distribution is based on the re-scaled Dgeo
       tmp.p.value <- 1 - pgamma(tmp.Dgeo, shape = current.a, rate = current.b)
-      to_keep <- tmp.p.value >= p_thres*(1-declining_rate^i)
+      to_keep <- tmp.p.value > min(tmp.p.value)
 
 
       if(all(tmp.p.value > p_thres)){
@@ -302,7 +334,7 @@ ggoutlier_geneticspace <- function(geo_coord, gen_coord = NULL, pgdM = NULL,
       i = i+1
 
     } # while loop end
-
+)
     ## collect results of all iterations
     collapse_res <- res.Iters[[1]]
     if(length(res.Iters) > 1){
@@ -323,7 +355,6 @@ ggoutlier_geneticspace <- function(geo_coord, gen_coord = NULL, pgdM = NULL,
         }
       }
     }
-
     logp.plot <- paste0(plot_dir, "/KNN_GenSP_test_multi_stage_Log10P_comparison.pdf")
     message(paste("the plot of comparing -logP between single-stage and multi-stage KNN tests is saved at ", logp.plot," \n", sep = ""))
     pdf(logp.plot, width = 4, height = 4.2)
@@ -337,8 +368,11 @@ ggoutlier_geneticspace <- function(geo_coord, gen_coord = NULL, pgdM = NULL,
     if(keep_all_stg_res){
       names(res.Iters) <- paste0("Iter_", 1:length(res.Iters))
       res.out <- c(res.Iters, collapse_res)
-      return(c(res.Iters, combined_result = list(collapse_res)))
+      out <- c(res.Iters, combined_result = list(collapse_res))
+      attr(out, "model") <- "ggoutlier_GeneticSpace"
+      return()
     }else{
+      attr(collapse_res, "model") <- "ggoutlier_GeneticSpace"
       return(collapse_res)
     }
   } # multi-stage testing end
