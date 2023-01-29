620735196418f76f3178b8ce3834bad34310619d
Che-Wei Chang <changc@uni-hohenheim.de>
2023-01-29 19:17:08 +0100

    minor changes in summary output format

--- ggoutlier/R/summary_ggoutlier.R
+++ ggoutlier/R/summary_ggoutlier.R
@@ -9,19 +9,19 @@ summary_ggoutlier <- function(ggoutlier_res){
   method <- attributes(ggoutlier_res)$model
   if(method == "composite"){
     ## get significant outliers from genetic space KNN result
-    sig_index <- ggoutlier_res$geneticKNN_result$statistics$significant
+    sig_index1 <- ggoutlier_res$geneticKNN_result$statistics$significant
     sig_geneticKNN <- data.frame(
-      "ID" = rownames(ggoutlier_res$geneticKNN_result$statistics)[sig_index],
-      "method" = "geneticKNN",
-      "p.value" = ggoutlier_res$geneticKNN_result$statistics[sig_index,"p.value"])
+      "ID" = rownames(ggoutlier_res$geneticKNN_result$statistics)[sig_index1],
+      "method" = rep("geneticKNN", sum(sig_index1)),
+      "p.value" = ggoutlier_res$geneticKNN_result$statistics[sig_index1,"p.value"])
     sig_geneticKNN <- sig_geneticKNN[order(sig_geneticKNN$p.value, decreasing = F),]
 
     ## get significant outliers from geographical space KNN result
-    sig_index <- ggoutlier_res$geoKNN_result$statistics$significant
+    sig_index2 <- ggoutlier_res$geoKNN_result$statistics$significant
     sig_geoKNN <- data.frame(
-      "ID" = rownames(ggoutlier_res$geoKNN_result$statistics)[sig_index],
-      "method" = "geoKNN",
-      "p.value" = ggoutlier_res$geoKNN_result$statistics[sig_index,"p.value"])
+      "ID" = rownames(ggoutlier_res$geoKNN_result$statistics)[sig_index2],
+      "method" = rep("geoKNN", sum(sig_index2)),
+      "p.value" = ggoutlier_res$geoKNN_result$statistics[sig_index2,"p.value"])
     sig_geoKNN <- sig_geoKNN[order(sig_geoKNN$p.value, decreasing = F),]
 
     prop_sig <- c(mean(ggoutlier_res$geneticKNN_result$statistics$significant),
@@ -35,6 +35,7 @@ summary_ggoutlier <- function(ggoutlier_res){
     sig_index <- ggoutlier_res$statistics$significant
     sig_geneticKNN <- data.frame(
       "ID" = rownames(ggoutlier_res$statistics)[sig_index],
+      "method" = rep("geoKNN", sum(sig_index)),
       "p.value" = ggoutlier_res$statistics[sig_index,"p.value"])
     sig_geneticKNN <- sig_geneticKNN[order(sig_geneticKNN$p.value, decreasing = F),]
     prop_sig <- c(mean(ggoutlier_res$geneticKNN_result$statistics$significant),
@@ -47,6 +48,7 @@ summary_ggoutlier <- function(ggoutlier_res){
     sig_index <- ggoutlier_res$geoKNN_result$statistics$significant
     sig_geoKNN <- data.frame(
       "ID" = rownames(ggoutlier_res$geoKNN_result$statistics)[sig_index],
+      "method" = rep("geneticKNN", nrow(ggoutlier_res$statistics)),
       "p.value" = ggoutlier_res$geoKNN_result$statistics[sig_index,"p.value"])
     sig_geoKNN <- sig_geoKNN[order(sig_geoKNN$p.value, decreasing = F),]
 
@@ -55,9 +57,9 @@ summary_ggoutlier <- function(ggoutlier_res){
     summary_out <- sig_geoKNN
   }
   rownames(summary_out) <- NULL
-  sig_ID_list <- unique(summary_out$ID)
+  #sig_ID_list <- unique(summary_out$ID)
   attr(summary_out, "proportion_of_outliers") = prop_sig
-  attr(summary_out, "significant_samples" = sig_ID_list)
-  class(summary_out) <- "ggoutlier_summary"
+  #attr(summary_out, "significant_samples" = sig_ID_list)
+  class(summary_out) <- c("data.frame", "ggoutlier_summary")
   return(summary_out)
 } # summary_ggoutlier end
